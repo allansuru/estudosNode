e80fdb95374e0f6df23af2b1f799d26e4e4b35c5
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-07-28 17:09:36 -0300

    manipulando get com node

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const app = express();
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
 
 
 app.get('/', (req, res) => {
@@ -8,8 +13,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([{'course A': 'c#'}, {'course B': 'Java'}, {'course C': 'Angular' }])
+    //res.send([{'course A': 'c#'}, {'course B': 'Java'}, {'course C': 'Angular' }])
+
+    res.send(courses);
 });
+
+//com parametros
+
+app.get('/api/courses/:id', (req, res) => {
+ // res.send(req.params.id); 
+ const course = courses.find(c => c.id === parseInt(req.params.id));
+ if (!course) res.status(404).send('The course with the given id was not found.')
+ res.send(course);
+});
+
+app.get('/api/posts/:year/:day', (req, res) => {
+    //res.send(req.params); 
+    res.send(req.query); 
+  });
 
 //PORT
 const port = process.env.PORT || 3000
