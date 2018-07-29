c0c01a6d438bbc7f731194c0539e4cdba119dc6a
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-07-29 19:18:31 -0300

    terminando o crud do express

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,6 +10,8 @@ const courses = [
     {id: 3, name: 'course3'}
 ];
 
+// GET
+
 
 app.get('/', (req, res) => {
     res.send('Oi');
@@ -35,19 +37,15 @@ app.get('/api/posts/:year/:day', (req, res) => {
     res.send(req.query); 
   });
 
+  // POST
 app.post('/api/courses', (req, res) => {
 
-    const schema = {
-        name: Joi.string().min(3).max(30).required()    
-    };
+
+    const { error } = validateCourse(req); // result.error
     
-    const result = Joi.validate(req.body, schema);
-    console.log(result.error)
-    //400 Bad request
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if(error)  return res.status(400).send(error.details[0].message);
+    
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -56,7 +54,45 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
+// PUT
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+ if (!course) return res.status(404).send('The course with the given id was not found.');
+ 
+    //Validate 
+    // If invalid, retunr 400
+
+    const { error } = validateCourse(req); // result.error
+
+    if(error) return res.status(400).send(error.details[0].message);
+    // Update course
+    // Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+//DELETE
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given id was not found.');
+    
+    let index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send('Deleted!');
+});
+
 //PORT
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Nodemon rodando na porta ${port}.`));
+
+function validateCourse(req) {
+    const schema = {
+        name: Joi.string().min(3).max(30).required()
+    };
+    return Joi.validate(req.body, schema);
+}
